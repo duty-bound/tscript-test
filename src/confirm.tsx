971e02c218d580b8d14c1eccf58d052e69ce77c1
Stephen <stephen.saliba@gmail.com>
2020-02-21 18:47:51 +0100

    chapter 2 or 3 ready

--- src/confirm.tsx
+++ src/confirm.tsx
@@ -1,18 +1,46 @@
 import * as React from 'react'
+import './confirm.css'
 
-export class Confirm extends React.Component {
+interface IProps {
+  open: boolean;
+  title: string;
+  content: string;
+  cancelCaption?: string;
+  okCaption?: string;
+  onOkClick: () => void;
+  onCancelClick: () => void;
+}
+
+export class Confirm extends React.Component<IProps> {
+
+  public static defaultProps = {
+    cancelCaption: 'Cancel',
+    okCaption: 'Okay'
+  }
+
+  private handleCancelClick = () => {
+    this.props.onCancelClick()
+  }
+
+  private handleOkClick = () => {
+    this.props.onOkClick()
+  }
+
   public render() {
     return (
-      <div className='confirm-wrapper confirm-visible'>
+      <div
+        className={this.props.open ? 'confirm-wrapper confirm-visible' : 'confirm-wrapper'}>
         <div className='confirm-container'>
-          <span>This is where our title should go</span>
-        </div>
-        <div className='confirm-content-container'>
-          <p>This is where our content should go</p>
-        </div>
-        <div className='confirm-buttons-container'>
-          <button className='confirm-cancel'>Cancel</button>
-          <button className='confirm-ok'>Okay</button>
+          <div className='confirm-title-container'>
+            <span>{this.props.title}</span>
+          </div>
+          <div className='confirm-content-container'>
+            <p>{this.props.content}</p>
+          </div>
+          <div className='confirm-buttons-container'>
+            <button className='confirm-cancel' onClick={this.handleCancelClick}>{this.props.cancelCaption}</button>
+            <button className='confirm-ok' onClick={this.handleOkClick}>{this.props.okCaption}</button>
+          </div>
         </div>
       </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -2,11 +2,92 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Confirm } from './confirm'
 
-const App: React.SFC = () => {
-  return <>
-      <h1>My React and TypeScript App!</h1>
-      <Confirm />
-    </>
+interface IState {
+  confirmOpen: boolean;
+  confirmMessage: string;
+  confirmVisible: boolean;
+  countDown: number;
+}
+
+class App extends React.Component<{}, IState> {
+
+  private timer: number = 0
+
+  constructor(props: {}) {
+    super(props)
+    this.state = {
+      confirmOpen: true,
+      confirmMessage: 'Please hit the confirm button',
+      confirmVisible: true,
+      countDown: 10,
+    }
+  }
+
+  private handleTimerTick() {
+    this.setState(
+      {
+        confirmMessage: `Please hit the confirm button ${
+          this.state.countDown
+        } secs to go`,
+        countDown: this.state.countDown - 1
+      },
+      () => {
+        if (this.state.countDown <= 0) {
+          clearInterval(this.timer);
+          this.setState({
+            confirmMessage: "Too late to confirm!",
+            confirmVisible: false
+          });
+        }
+      }
+    );
+  }
+
+  private handleConfirmClick = () => {
+    this.setState({ confirmOpen: true })
+    clearInterval(this.timer)
+  }
+
+  private handleCancelConfirmClick = () => {
+    this.setState({
+      confirmMessage: 'Take a break, I am sure you will later...',
+      confirmOpen: false
+    })
+    clearInterval(this.timer)
+  }
+
+  private handleOkConfirmClick = () => {
+    this.setState({
+      confirmMessage: 'Cool, carry on reading!',
+      confirmOpen: false
+    })
+    clearInterval(this.timer)
+  }
+
+  public render() {
+    return (
+        <React.Fragment>
+          <h1>My React and TypeScript App!</h1>
+          <p>{this.state.confirmMessage}</p>
+          {this.state.confirmVisible && (
+            <button onClick={this.handleConfirmClick}>Confirm</button>
+          )}
+          <Confirm
+            open={this.state.confirmOpen}
+            title='React and TypeScript'
+            content='Are you sure you want to learn React and TypeScript?'
+            cancelCaption='No way'
+            okCaption='Yes please!'
+            onCancelClick={this.handleCancelConfirmClick}
+            onOkClick={this.handleOkConfirmClick}
+          />
+        </React.Fragment>
+    )
+  }
+
+  public componentDidMount() {
+    this.timer = window.setInterval(() => this.handleTimerTick(), 1000)
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement)
